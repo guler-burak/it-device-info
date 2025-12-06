00d4dafde2f091970f0381e172cbc6c9200385f8
Burak Guler <burak@192.168.1.108>
2025-12-06 11:51:22 +0300

    Option 3 added: retrieving last reboot time

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import wmi
+import psutil
+from datetime import datetime
 
 def get_pc_brand_model():
     c = wmi.WMI()
@@ -10,12 +12,20 @@ def get_serial_number():
     bios = c.Win32_BIOS()[0]
     return bios.SerialNumber
 
+def last_reboot():
+    t = psutil.boot_time()
+    dt = datetime.fromtimestamp(t)
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
 if __name__ == "__main__":
     print("Welcome to the Device Info App\n")
     print("1 - Show Device Brand and Model")
     print("2 - Show Device Serial Number\n")
+    print("3 - Show Last Reboot Time\n")
+    
 
-    choice = input("Press 1 or 2 to continue: ")
+    choice = input("Press 1, 2 or 3 to continue: ")
 
     if choice == "1":
         brand, model = get_pc_brand_model()
@@ -25,6 +35,10 @@ if __name__ == "__main__":
     elif choice == "2":
         serial = get_serial_number()
         print("\nSerial Number:", serial)
+    
+    elif choice == "3":
+        reboot = last_reboot()
+        print("\nReboot Time:", reboot)
 
     else:
         print("\nInvalid choice")
